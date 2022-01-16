384490a0010955101821a170ea00da047e48c4ba
Scott Cho <scottsgcho@gmail.com>
2022-01-16 18:15:28 +0900

    separate out reused contracts

---
+++ scripts/deployArt.ts
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function main(): Promise<void> {
+    // Deploy ART
+    const ART = await ethers.getContractFactory('ArtERC20Token');
+    const art = await ART.deploy();
+
+    
+}
+
+main()
+    .then(() => process.exit())
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+})
