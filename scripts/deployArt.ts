e41b692243936840520e61b8d2f44ba575156806
Scott Cho <scottsgcho@gmail.com>
2022-01-16 23:17:17 +0900

    frax bond depository

--- scripts/deployArt.ts
+++ scripts/deployArt.ts
@@ -4,7 +4,8 @@ async function main(): Promise<void> {
     // Deploy ART
     const ART = await ethers.getContractFactory('ArtERC20Token');
     const art = await ART.deploy();
-
+    await art.deployed()
+    
     
 }
 
